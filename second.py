5f38e5af54c9cd081af9d68904a7de42b557df0a
Rehan Khan <rehankhan7209658@gmail.com>
2021-12-22 22:34:58 +0530

    created a calculator in python

--- second.py
+++ second.py
@@ -1,15 +1,3 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-salary = float(input("Enter your salary: "))
-unemployed = bool(input("Are you unemployed? "))
-
-print(name)
-print(age)
-print(salary)
-print(unemployed)
-
-s1 = "1"
-s2 = ""
-
-print(bool(s1))
-print(bool(s2))
+#calculator in python
+string = input("Enter your exression: ")
+print("result: " + str(eval(string)))
