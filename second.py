4210e553053c7cedef88e9dcdaf7886773d928e6
Rehan Khan <rehankhan7209658@gmail.com>
2021-12-22 22:11:11 +0530

    learned formated string in python

--- second.py
+++ second.py
@@ -1,5 +1,7 @@
-a = 54
-x , y = 5, 6
-p = 456; q = 23498
-
-print(a, x, y, p, q)
+a = [1,2,3,4,5]
+name = 'Vishal Khan'
+name2 = "Ezaz Kumar"
+age = 60
+print("My name is %s. And my friend's name is %s. My age is %i" % (name, name2, age))
+print(f"My name is {name}. And my friend's name is {name2}.My age is {age}")
+print(a,"Gaurav", "Kumar")
