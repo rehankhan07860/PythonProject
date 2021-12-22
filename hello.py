2325c883e93995d6dafce95b2c91ff3e86883f24
Rehan Khan <rehankhan7209658@gmail.com>
2021-12-22 18:07:46 +0530

    second day of python

--- hello.py
+++ hello.py
@@ -1,15 +1,1 @@
-#making function in python
-def mul(x,y):
-    """
-    This is a function namely mul that takes two numbers as input and returns the multiplication of the numbers.
-    """
-    return x * y
-
-def sum(x,y,z):
-    '''
-    This is a function that takes  threee numbers as innput and returns the sum of the numbers
-    '''
-    return x + y + z
-
-print(sum(2,4,5))
-print(mul(2,3))
+print("Rehan khan")
