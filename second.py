3146e001c2637ec3bbe7e88e640ce986e7af4e67
Rehan Khan <rehankhan7209658@gmail.com>
2021-12-22 22:27:20 +0530

    taking input in python

--- second.py
+++ second.py
@@ -1,7 +1,15 @@
-a = [1,2,3,4,5]
-name = 'Vishal Khan'
-name2 = "Ezaz Kumar"
-age = 60
-print("My name is %s. And my friend's name is %s. My age is %i" % (name, name2, age))
-print(f"My name is {name}. And my friend's name is {name2}.My age is {age}")
-print(a,"Gaurav", "Kumar")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+salary = float(input("Enter your salary: "))
+unemployed = bool(input("Are you unemployed? "))
+
+print(name)
+print(age)
+print(salary)
+print(unemployed)
+
+s1 = "1"
+s2 = ""
+
+print(bool(s1))
+print(bool(s2))
