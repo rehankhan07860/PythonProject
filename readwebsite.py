0ff9f0c0b81261422bb468871cd3a219947755e0
Rehan Khan <rehankhan7209658@gmail.com>
2021-12-24 21:49:43 +0530

    read w3website

--- readwebsite.py
+++ readwebsite.py
@@ -1,7 +1,9 @@
 import urllib.request as urq
 
 website = 'https://www.w3schools.com/html/default.asp'
-
 file = urq.urlopen(website)
 
-print(file.read())
+content = file.read()
+
+f = open('myfile.html', 'wb')
+f.write(content)
